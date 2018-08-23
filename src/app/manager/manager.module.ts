e73d0c497d1512d957bcee463237342c21abd0bf
Qi Ning <qning@expedia.com>
2018-08-23 10:11:40 -0700

    Added routing for manager modules user management, etc

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
+import { MaterialModule } from '../material.module';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    MaterialModule,
+    RouterModule
   ],
-  declarations: [ManagerHomeComponent]
+  declarations: [
+    ManagerHomeComponent,
+    ManagerComponent,
+    UserManagementComponent,
+    ReceiptLookupComponent
+  ]
 })
 export class ManagerModule { }
